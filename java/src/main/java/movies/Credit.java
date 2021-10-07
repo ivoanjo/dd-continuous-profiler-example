124c6eb6f0a88cf03af8d27b9939256e19e32e08
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-10-07 15:50:56 +0100

    Fix credits database

--- java/src/main/java/movies/Credit.java
+++ java/src/main/java/movies/Credit.java
@@ -11,24 +11,24 @@ import org.bson.Document;
 public final class Credit {
 
 	public final Integer id;
-	public final String crew;
-	public final String cast;
+	public final List<String> crew;
+	public final List<String> cast;
 
-	private Credit(JSONObject data) {
-		this.id = Integer.parseInt((String)data.get("id"));
-		this.crew = (String)data.get("crew");
-		this.cast = (String)data.get("cast");
-	}
+	// private Credit(JSONObject data) {
+	// 	this.id = Integer.parseInt((String)data.get("id"));
+	// 	this.crew = (String)data.get("crew");
+	// 	this.cast = (String)data.get("cast");
+	// }
 
 	public Credit(Document data) {
 		this.id = Integer.parseInt(data.getString("id"));
-		this.crew = data.getString("crew");
-		this.cast = data.getString("cast");
+		this.crew = data.getList("crew", String.class);
+		this.cast = data.getList("cast", String.class);
 	}
 
-	private static List<Credit> parse() {
-		return Parser.parse(Path.of("credits.json"), Credit::new);
-	}
+	// private static List<Credit> parse() {
+	// 	return Parser.parse(Path.of("credits.json"), Credit::new);
+	// }
 
 	// public static List<Credit> getAll() {
 	// 	return parse();
